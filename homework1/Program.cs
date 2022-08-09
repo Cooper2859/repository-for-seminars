4150cd3d44a27b4c0214dd58eded8a9136deb80f
Evgeny Khryashchev <master_2859@rambler.ru>
2022-08-09 21:51:40 +0300

    add Program.cs_1

---
+++ homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1
+int numA;
+int numB;
+
+Console.Write("Input first number: " );
+numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: " );
+numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine ($"max = {numA}");
+}
+else 
+{
+    Console.WriteLine ($"max = {numB}");
+}
