2d1798677ee8a502e1c0eb7bfecb3dcfb4fdffc4
Evgeny Khryashchev <master_2859@rambler.ru>
2022-08-12 16:41:04 +0300

    add task1

--- Seminar_2/Task1/Program.cs
+++ Seminar_2/Task1/Program.cs
@@ -26,6 +26,7 @@ else {
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
+/*
 Console.Write("Введи цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -41,3 +42,4 @@ void CheckingTheDayOfTheWeek (int dayNumber)
 }
 
 CheckingTheDayOfTheWeek(dayNumber);
+/*
