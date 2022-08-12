eea0fc209e12b00d58c5aea5064865106edbe5ec
Evgeny Khryashchev <master_2859@rambler.ru>
2022-08-12 16:34:02 +0300

    add task1

--- Seminar_2/Task1/Program.cs
+++ Seminar_2/Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write ("Введите трехзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
