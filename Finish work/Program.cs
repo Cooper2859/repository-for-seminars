3ebfb35a3a977392189bb199ac3f5f7f048223cd
Evgeny Khryashchev <master_2859@rambler.ru>
2022-09-20 12:04:28 +0300

    add Finish work

--- Finish work/Program.cs
+++ Finish work/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] animals = {"Cat", "Dog", "Mouse", "Pig", "Sheep", "Hen", "Cow", "Horse", "Rabbit", "Hamster"};
+string[] result = new string[animals.Length];
+
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void Print(string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.Write(mas[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+NewArray(animals, result);
+Print(animals);
+Console.WriteLine();
+Print(result);
